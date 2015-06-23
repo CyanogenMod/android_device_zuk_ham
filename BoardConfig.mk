19690c2d028f27c5bfe99c7c71f30ae0c99f555b
Diogo Ferreira <defer@cyngn.com>
2015-06-23 17:47:51 +0100

    ham: Use the common 8974 kernel.
    Change-Id: Ib25b0d527ed5995cc6f377613f2089630a96a2a5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_ARCH := arm
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b6 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=480M
-TARGET_KERNEL_SOURCE := kernel/zuk/ham
+TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8974
 TARGET_KERNEL_CONFIG := cyanogenmod_k9_defconfig
 
 # QCOM
