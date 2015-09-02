0156b83430565ecae78221c5b3e4f47316f9f464
Diogo Ferreira <defer@cyngn.com>
2015-09-02 16:12:43 +0100

    ham: Use the notification led for battery as well
    Change-Id: I2fb1458befe356ab8511005b086a57eb9e7c266a
    Ticket: HAM-299

--- liblight/lights.c
+++ liblight/lights.c
@@ -370,6 +370,20 @@ set_light_buttons(struct light_device_t* dev,
     return err;
 }
 
+static int
+set_light_battery(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    pthread_mutex_lock(&g_lock);
+
+    g_battery = *state;
+    handle_speaker_battery_locked(dev);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return 0;
+}
+
 /** Close the lights device */
 static int
 close_lights(struct light_device_t *dev)
@@ -402,6 +416,8 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
+    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
+        set_light = set_light_battery;
     else
         return -EINVAL;
 
