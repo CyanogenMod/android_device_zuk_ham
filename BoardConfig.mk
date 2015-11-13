3f9d7544c32d2c85b95da2f664b7f928c0971f9f
Diogo Ferreira <defer@cyngn.com>
2015-11-13 15:37:55 +0000

    ham: Disable EAP-SIM for now
    The wpa-supplicant component isn't quite ready for OSS builds yet.
    
    Change-Id: Ic3c5a0f830cab5e5bd11b5cbba2fbc97cbfbfc7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,8 +131,8 @@ TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
 # Wifi - EAP-SIM
-CONFIG_EAP_PROXY                 := qmi
-CONFIG_EAP_PROXY_DUAL_SIM        := true
+# CONFIG_EAP_PROXY                 := qmi
+# CONFIG_EAP_PROXY_DUAL_SIM        := true
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
