92047f020a3d6189b82abd41119b5cb9b55b8b77
Diogo Ferreira <defer@cyngn.com>
2016-01-11 12:31:32 +0000

    ham: Only dex pre-opt the boot classpath
    Change-Id: I7db0dce38721c9c6ad67695d6e9820e15188bdc0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,13 +171,7 @@ BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
 # inherit from the proprietary version
 ifneq ($(QCPATH),)
