928344825f14f6056038d2ed3bfe86efba9b9a72
Diogo Ferreira <deovferreira@gmail.com>
2016-01-11 07:57:06 -0800

    Revert "ham: Only dex pre-opt the boot classpath"
    This reverts commit c35e209a185ec623140f9f5e059c4d772c3cbb00.
    
    Ticket: HAM-1231
    Change-Id: I201751dba7f8ac2269449fc746bf56c9c2ac4d24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,13 @@ BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+ifeq ($(HOST_OS),linux)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
 
 # inherit from the proprietary version
 ifneq ($(QCPATH),)
