8871a74db1928c4cd217d31fe636ac87fdf01dd1
MrColdbird <gentleman.trilby@gmail.com>
2016-01-26 15:27:46 +0100

    Pulled in sepolicy from official CM13 (it will make a good base to work from)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,10 +45,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_ARCH := arm
-BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=480M androidboot.selinux=permissive
-
-# The CM-kernel will not boot on Android 6.0, but we leave this in place so that we trigger the source-building of the kernel
-# The actual Chroma-kernel source is getting pulled via a local manifest
+BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=480M
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8974
 TARGET_KERNEL_CONFIG := cyanogenmod_k9_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
