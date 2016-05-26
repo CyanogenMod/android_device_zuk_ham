7d15d166fac576a64e5c4632e4d581aefca259f9
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-05-26 16:00:43 +0800

    ham: Use power config for recovery
    Move the recovery-related configs into the power config,
    and copy the power config to recovery.
    
    Thus, we can get rid of the dedicated recovery script.
    
    Change-Id: I8603157f37e99999fa68bdaae68c7e4bd002b0be

--- ham.mk
+++ ham.mk
@@ -33,8 +33,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
     init.qcom.power.rc \
-    init.qcom.usb.rc \
-    init.recovery.qcom.rc
+    init.qcom.usb.rc
 
 # Audio
 PRODUCT_COPY_FILES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,6 +54,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
